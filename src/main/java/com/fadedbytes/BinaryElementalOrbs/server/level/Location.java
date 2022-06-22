4464db4c46b5f6abf47c3fc5441925b302f8ddec
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-06-23 01:51:43 +0200

    Location class added. It can provide location instances for level elements and limits

--- src/main/java/com/fadedbytes/BinaryElementalOrbs/server/level/Location.java
+++ src/main/java/com/fadedbytes/BinaryElementalOrbs/server/level/Location.java
@@ -15,18 +15,49 @@ public class Location {
         }
     }
 
-    public float getCoord(int i) {
-        if (coords.length <= i) return 0.0f;
-        return coords[i];
+    /**
+     * Returns the value of the coordinate at the specified axis position, or 0 if the axis does not exist.
+      * @param axisPosition the value of the axis to get.
+     * @return the value of the specified axis in the current location.
+     */
+    public float getCoord(int axisPosition) {
+        if (coords.length <= axisPosition) return 0.0f;
+        return coords[axisPosition];
+    }
+
+    /**
+     * Returns the value of the coordinate at the specified axis, or 0 if the axis does not exist.
+     * @param axis the axis to get the value of.
+     * @return the value of the specified axis in the current location.
+     */
+    public float getCoord(Axis axis) {
+        return getCoord(axis.getPosition());
+    }
+
+    public Level getLevel() {
+        return level;
     }
 
 
+    /**
+     * Some predefined axis positions.
+     */
+    public enum Axis {
+        X(0),
+        Y(1),
+        Z(2),
+        W(3);
 
-    public static enum AXIS {
-        X,
-        Y,
-        Z,
-        W
+        final int axisPosition;
+
+        Axis(int axisPosition) {
+            this.axisPosition = axisPosition;
+        }
+
+        int getPosition() {
+            return this.axisPosition;
+        }
+
     }
 
 }
