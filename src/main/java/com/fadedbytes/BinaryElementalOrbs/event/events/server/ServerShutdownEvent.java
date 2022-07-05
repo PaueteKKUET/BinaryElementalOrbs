dbf6d48b4dc83a12545d8b9aa2ef9442cc700522
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-07-05 22:04:54 +0200

    Implemented the timestampedevent interface

--- src/main/java/com/fadedbytes/BinaryElementalOrbs/event/events/server/ServerShutdownEvent.java
+++ src/main/java/com/fadedbytes/BinaryElementalOrbs/event/events/server/ServerShutdownEvent.java
@@ -1,15 +1,24 @@
 package com.fadedbytes.BinaryElementalOrbs.event.events.server;
 
+import com.fadedbytes.BinaryElementalOrbs.event.events.TimestampedEvent;
 import com.fadedbytes.BinaryElementalOrbs.server.BeoServer;
 import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
 
 /**
  * Fired when a server is about to shut down.
  * @param server The server that is about to shut down.
+ * @param timestamp The date and time the server is about to shut down.
  */
-public record ServerShutdownEvent(BeoServer server) implements ServerEvent {
+public record ServerShutdownEvent(BeoServer server, LocalDateTime timestamp) implements ServerEvent, TimestampedEvent {
     @Override
     public @NotNull BeoServer server() {
         return this.server;
+    }
+
+    @Override
+    public @NotNull LocalDateTime timestamp() {
+        return this.timestamp;
     }
 }
--- src/main/java/com/fadedbytes/BinaryElementalOrbs/event/events/server/ServerStartupEvent.java
+++ src/main/java/com/fadedbytes/BinaryElementalOrbs/event/events/server/ServerStartupEvent.java
@@ -1,22 +1,24 @@
 package com.fadedbytes.BinaryElementalOrbs.event.events.server;
 
+import com.fadedbytes.BinaryElementalOrbs.event.events.TimestampedEvent;
 import com.fadedbytes.BinaryElementalOrbs.server.BeoServer;
 import org.jetbrains.annotations.NotNull;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * Fired when a server just started.
  * @param server The server that just started.
- * @param startupDate The date and time the server just started.
+ * @param timestamp The date and time the server just started.
  */
-public record ServerStartupEvent(BeoServer server, LocalDate startupDate) implements ServerEvent {
+public record ServerStartupEvent(BeoServer server, LocalDateTime timestamp) implements ServerEvent, TimestampedEvent {
     @Override
     public @NotNull BeoServer server() {
         return this.server;
     }
 
-    public @NotNull LocalDate startupDate() {
-        return this.startupDate;
+    @Override
+    public @NotNull LocalDateTime timestamp() {
+        return this.timestamp;
     }
 }
