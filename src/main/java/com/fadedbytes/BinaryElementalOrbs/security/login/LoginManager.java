18d209129f4aecbcc664a94d096b66e1ee693b39
Pau Salanova Blanco <pauete.sb@gmail.com>
2022-07-05 22:12:46 +0200

    Added LoginManager in order to manage the security of the server connections. Not working right now, needs to be implemented with a database

---
+++ src/main/java/com/fadedbytes/BinaryElementalOrbs/security/login/LoginManager.java
@@ -0,0 +1,21 @@
+package com.fadedbytes.BinaryElementalOrbs.security.login;
+
+import com.fadedbytes.BinaryElementalOrbs.server.player.Player;
+
+public class LoginManager {
+
+    public static synchronized boolean isCorrectPassword(Player player, String password) {
+        // TODO: Implement
+        return true;
+    }
+
+    public static synchronized boolean playerExists(String playerName) {
+        // TODO: Implement
+        return false;
+    }
+
+    public static synchronized void registerPlayer() {
+        // TODO: Implement
+    }
+
+}
